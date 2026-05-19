bdfee6c7d600a6f8edfc1ae8e748526148e0cfd2
RJ.P <329rpodlas@frhsd.com>
2026-05-19 09:25:24 -0300

    Add files via upload

---
+++ ai_studio_code.js
@@ -0,0 +1,13 @@
+function searchGames() {
+    let input = document.getElementById('gameSearch').value.toLowerCase();
+    let cards = document.getElementsByClassName('game-card');
+
+    for (let i = 0; i < cards.length; i++) {
+        let title = cards[i].getElementsByTagName('h3')[0].innerText.toLowerCase();
+        if (title.includes(input)) {
+            cards[i].style.display = "";
+        } else {
+            cards[i].style.display = "none";
+        }
+    }
+}
